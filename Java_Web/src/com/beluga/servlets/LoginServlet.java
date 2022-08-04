1b2e8dc3b98b5917e40b51883512f4c254ec9181
BelugaInCoding <shuh_fun@outlook.com>
2022-08-04 23:32:41 +0800

    20220804

---
+++ Java_Web/src/com/beluga/servlets/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.beluga.servlets; /**
+ * @author Beluga
+ * @createTime 2022/8/4 -- 22:09
+ */
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String name = request.getParameter("uName");
+        String pwd = request.getParameter("pwd");
+
+        System.out.println("登陆账号为："+name);
+        System.out.println("登陆密码为："+pwd);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
