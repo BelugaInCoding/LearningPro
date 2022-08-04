5f0d1abf84760fd87ab0faa0b585b859c4fa812e
BelugaInCoding <shuh_fun@outlook.com>
2022-08-04 23:35:33 +0800

    20220804

--- Java_Web/src/com/beluga/servlets/RegisterServlet.java
+++ Java_Web/src/com/beluga/servlets/RegisterServlet.java
@@ -1,23 +1,25 @@
-package com.beluga.servlets;
+package com.beluga.servlets; /**
+ * @author Beluga
+ * @createTime 2022/8/4 -- 22:17
+ */
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 
-/**
- * @author Beluga
- * @createTime 2022/8/1 -- 22:13
- */
 public class RegisterServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("uName");
-        String pwd = req.getParameter("pwd");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String name = request.getParameter("uName");
+        String pwd = request.getParameter("pwd");
 
-        System.out.println(name);
-        System.out.println(pwd);
+        System.out.println("注册注册账号为："+name);
+        System.out.println("注册密码为："+pwd);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
     }
 }
