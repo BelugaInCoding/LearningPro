04fd4f9de330e09525106f13e391094436ea69cd
BelugaInCoding <shuh_fun@outlook.com>
2022-11-02 23:56:44 +0800

    2022/11/02

--- Java_Web/src/com/beluga/test/UserDaoTest.java
+++ Java_Web/src/com/beluga/test/UserDaoTest.java
@@ -2,23 +2,36 @@ package com.beluga.test;
 
 import com.beluga.dao.UserDao;
 import com.beluga.dao.impl.UserDaoImpl;
+import com.beluga.pojo.User;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
  * @author Beluga
- * @createTime 2022/8/15 -- 23:43
+ * @createTime 2022/11/1 -- 22:40
  */
 public class UserDaoTest {
 
+    UserDao userdao = new UserDaoImpl();
+
     @Test
     public void queryUserByUsername() {
-        UserDao userDao = new UserDaoImpl();
-        System.out.println(userDao.queryUserByUsername("admin"));;
+        if (userdao.queryUserByUsername("admin123") == null){
+            System.out.println("用户名可用");
+        } else {
+            System.out.println("用户名不可用");
+        }
+    }
+
+    @Test
+    public void queryUserByUsernameAndPassword() {
+        System.out.println(userdao.queryUserByUsernameAndPassword("admin","admin"));
     }
 
     @Test
     public void saveUser() {
+        System.out.println(userdao.saveUser(new User(
+                null, "iwen", "123456","testEmail")));
     }
 }
